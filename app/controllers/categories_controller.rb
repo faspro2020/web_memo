526bf86f4176de3a84ed671e48573f2bb8d9e3fe
faspro2020 <66356762+faspro2020@users.noreply.github.com>
2020-08-23 22:44:29 +0900

    コントローラの設定

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,14 @@ class CategoriesController < ApplicationController
   def index
     @category = Category.all
   end
+
+  def create
+    Category.create(category_params)
+  end
+
+  private
+
+  def category_params
+    params.require(:category).parmit(:name, :comment)
+  end
 end
